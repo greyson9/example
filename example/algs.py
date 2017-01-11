f5407fc5228767cbac26ae406be5b79996078067
Greyson Lewis <greyson.lewis@ucsf.edu>
2017-01-11 12:10:04 -0800

    added bubblesort

--- example/algs.py
+++ example/algs.py
@@ -8,10 +8,30 @@ def pointless_sort(x):
     return np.array([1,2,3])
 
 def bubblesort(x):
-    """
-    Describe how you are sorting `x`
-    """
+# input: a list of integers, x
+# output: a sorted list (low to high) with the elements of x
+# first, check to make sure the list is of appropriate size and content
+# then, for each adjacent pair of elements, swap them 
+# and store the current index (the rest of the list is already sorted)
+# if the current index is 0, then nothing remains to be sorted
 
+    if len(x) == 0:
+        print('Zero-length list')
+        return []
+    elif any(not isinstance(mem, int) for mem in x):
+        print('List contains non-integer element')
+        return []
+    else:
+        n = len(x)
+        while n > 0:
+            last_sorted = 0
+            for i in range(1, n):
+                if x[i-1] > x[i]:
+                    temp = x[i-1]
+                    x[i-1] = x[i]
+                    x[i] = temp
+                    last_sorted = i
+            n = last_sorted
     assert 1 == 1
     return x
 
